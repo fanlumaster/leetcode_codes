d70f052e6f157c6f6f8105f01b206477da34f40d
fanlumaster <1730976608@qq.com>
2021-07-09 18:25:23 +0800

    Initial commit

---
+++ draft/draft01.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @File  : draft01.py
+# @Author: FanLu
+# @Date  : 2021/7/9
+
+a = [1, 2, 3]
+print(type(a))
---
+++ first100/leet_01.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# @File  : leet_01.py
+# @Author: FanLu
+# @Date  : 2021/7/9
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(0, len(nums)):
+            dic[nums[i]] = i
+        for i in range(0, len(nums)):
+            complement = target - nums[i]
+            if complement in dic.keys() and dic[complement] != i:
+                return [i, dic[complement]]
+        return [] # 不存在的情况
+
+class Solution3:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(0, len(nums)):
+            complement = target - nums[i]
+            if complement in dic.keys():
+                return [dic[complement], i]
+            dic[nums[i]] = i
+
+if __name__ == '__main__':
+    solution = Solution()
+    nums = [3, 2, 4]
+    target = 6
+    res = solution.twoSum(nums, target)
+    print(res)
+
